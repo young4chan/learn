1315fefafbad2c780f11ad8e04637d906d24d99a
Chen Yang <young.chan@live.com>
2019-03-02 17:41:07 +0800

    modified socket_client.pl

--- learnperl/socket_client.pl
+++ learnperl/socket_client.pl
@@ -7,14 +7,15 @@ use Socket;
 # initialize host and port
 my $host = shift || 'localhost';
 my $port = shift || 7890;
-my $server = "localhost";    # Host IP running the server
+my $server = "192.168.1.2";    # Host IP running the server
 
 # create the socket, connect to the port
 socket(SOCKET, PF_INET, SOCK_STREAM, (getprotobyname('tcp'))[2]) or die "Can't create a socket $!\n";
 connect(SOCKET, pack_sockaddr_in($port, inet_aton($server))) or die "Can't connect to port $port \n";
 
-my $line;
-while ($line = <SOCKET>) {
-	print "$line\n";
-}
+# my $line;
+# while ($line = <SOCKET>) {
+# 	print "$line\n";
+# }
+print SOCKET "I'm Client\n";
 close SOCKET or die "close: $!";
